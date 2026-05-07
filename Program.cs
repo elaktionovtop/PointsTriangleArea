9ad0523e1d3692cc48e2a8dd31e8d15d86b1df35
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-05-07 10:00:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+C# for Grandchildren: educational console application
+Площадь треугольника по координатам (PointsTriangleArea)
+
+Задача: 
+Треугольник задан координатами своих вершин: (x1, y1), (x2, y2), (x3, y3).
+Вычислить площадь треугольника.
+*/
+
+/*
+*/
+using static System.Console;
+using static ConsoleLibrary.ConsoleApp;
+using static ConsoleLibrary.ConsoleIO;
+
+WriteAppTitle("Площадь треугольника по координатам");
+Run(Action);
+PromptExitApp();
+
+void Action()
+{
+}
